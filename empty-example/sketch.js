ecaa109855084dc0cf23959e4e795aee022027ed
Mani bhargavi <manibhargavi@Manis-MacBook-Pro.local>
2016-02-13 00:21:12 -0500

    awesomeness

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -8,33 +8,39 @@ var canvas;
 var grabtext;
 var start;
 var startscreen = true;
-
+var back;
 function setup() {
 	
 	bg = loadImage("cloudbackground.png");
 	canvas = createCanvas(1280, 800);
  	grabtext = loadImage("grabthecupcaketext.png");
  	start = loadImage("start.png");
+ 	back = loadImage("back.png");
 }
 
 function draw() {
 	if(startscreen){
  	background(bg);
-    image(grabtext,120, height/4);
-    image(start,width/2-100, height/2);
+    image(grabtext,130, height/3);
+    image(start,width/2-90, height/3+25);
 	}
 	else{
-	background(bg);	
+	background(bg);
+	image(back,10,10,40,40);
 	}
 }
 
 function mousePressed() {
-var d = dist(mouseX,mouseY,590,422);
-if(d<60){
-	startscreen = false;
-	// var text = 'x='+mouseX.toString()+', y='+mouseY.toString();
-	// alert(text);
-
-}
-
+	//var text = 'x='+mouseX.toString()+', y='+mouseY.toString();
+	//alert(text);
+	var d_start = dist(mouseX,mouseY,600,500);
+	if(d_start<60){
+		startscreen = false;
+		// var text = 'x='+mouseX.toString()+', y='+mouseY.toString();
+		// alert(text);
+	}
+	var d_back = dist(mouseX,mouseY,10,10)
+	if(d_back<50) {
+		startscreen = true;
+	}
 }
